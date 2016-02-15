a15476db1aeb614d7ca8484a51298c6c39a18aaf
patrickblom <p.blom@shopmacher.de>
2016-02-15 10:55:51 +0100

    - remove jquery from inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,40 +1,106 @@
-$(document).ready(function () {
+var inspectorJira = function () {
 
-    // set basics
-    var applicationInfo = {
-        "isJira": false
+    /**
+     * Identifier Class
+     *
+     * @param app
+     * @constructor
+     */
+    var JriaIdentifier = function (app) {
+        this.app = (app == null) ? document : app;
+        this.isJira = false;
+        this.applicationData = {
+            "devMode": false,
+            "version": '',
+            "build": '',
+            "isBeta": false,
+            "isRC": false,
+            "provideMails": false
+        }
+
     };
 
-    // check if current page is a jira
-    if ($('meta[name=application-name]').attr('content') == 'JIRA') {
-        applicationInfo.isJira = true;
-    }
+    JriaIdentifier.prototype = {
 
-    // fetch some informations
-    if (applicationInfo.isJira) {
-        var metaTags = {
-            "devMode": "ajs-dev-mode",
-            "version": "ajs-version-number",
-            "build": "ajs-build-number",
-            "isBeta": "ajs-is-beta",
-            "isRC": "ajs-is-rc",
-            "provideMails": "ajs-outgoing-mail-enabled"
-        };
-
-        $.each(metaTags, function (index, item) {
-
-            var value = $('meta[name=' + item + ']').attr('content');
-            if (value === "true" || value === "false") {
-                value = JSON.parse(value);
+        /**
+         * Identifies the given document for a jira instance
+         *
+         * @returns {boolean}
+         */
+        identify: function () {
+            if (this.getMetaContentByName('application-name') == 'JIRA') {
+                this.isJira = true;
             }
 
-            applicationInfo[index] = value;
-        });
+            return this.isJira;
+        },
 
+        /**
+         * Get the meta elements from current app
+         *
+         * @param name
+         * @param content
+         * @returns {string}
+         */
+        getMetaContentByName: function (name, content) {
+            var content = (content == null) ? 'content' : content;
+            return this.app.querySelector("meta[name='" + name + "']").getAttribute(content);
+        },
+
+        /**
+         * Fetch the application data from DOM
+         *
+         * @returns {{devMode: boolean, version: string, build: string, isBeta: boolean, isRC: boolean, provideMails: boolean}|*}
+         */
+        getApplicationData: function () {
+
+            var metaTags = this.getMetaTags();
+
+            for (var property in metaTags) {
+                var value = this.getMetaContentByName(metaTags[property]);
+                // crapy but ist works
+                if (value === "true" || value === "false") {
+                    value = JSON.parse(value);
+                }
+
+                this.applicationData[property] = value;
+
+            }
+
+            return this.applicationData;
+        },
+
+        /**
+         * returns the jira specific metatags
+         *
+         * @returns {{devMode: string, version: string, build: string, isBeta: string, isRC: string, provideMails: string}}
+         */
+        getMetaTags: function () {
+
+            var metaTags = {
+                "devMode": "ajs-dev-mode",
+                "version": "ajs-version-number",
+                "build": "ajs-build-number",
+                "isBeta": "ajs-is-beta",
+                "isRC": "ajs-is-rc",
+                "provideMails": "ajs-outgoing-mail-enabled"
+            };
+
+            return metaTags;
+        }
+    };
+
+    // start the main script
+    var jindentifier = new JriaIdentifier();
+
+    if (jindentifier.identify()) {
         // send infos to the background script
         chrome.runtime.sendMessage({
-            "applicationInfo": applicationInfo
+            "applicationInfo": jindentifier.getApplicationData()
         });
-
     }
-});
+
+}();
+
+// load inspector jira
+document.addEventListener('DOMContentLoaded', inspectorJira, false);
