2a982ca03f387997d0245e8f9af511f43b70667e
patrickblom <blomsmail@gmail.com>
2016-02-15 21:08:40 +0100

    - remove helper js - start re-engineering page_action.js

--- src/page_action/page_action.js
+++ src/page_action/page_action.js
@@ -1,3 +1,51 @@
+var page_action = function () {
+
+    var PopUp = function (headline, headlinestate, itemcontainer) {
+        this.headLine = headline;
+        this.headLineState = headlinestate;
+        this.itemcontainer = itemcontainer;
+    };
+
+    PopUp.prototype = {
+        generate: function () {
+
+        },
+        /**
+         * Gets the color css class for the template
+         *
+         * @param itemValue
+         * @returns {string}
+         */
+        getColorClass: function (itemValue) {
+
+            // set deafult
+            var color = 'bg-info';
+
+            // decide if green or red
+            if (itemValue === true || itemValue == false) {
+                color = (itemValue === true) ? 'bg-success' : 'bg-danger';
+            }
+
+            return color;
+        },
+        /**
+         *
+         * @param key the property identifier
+         * @param value the property value
+         * @param label the localized label
+         * @param color the generated color
+         * @returns {string}
+         */
+        getItemTemplate: function (key, value, label, color) {
+            return  '<li class="property">' +
+                        '<span class="jira-label ' + key + '">' + label + '</span>' +
+                        '<span class="jira-badge round ' + color + '">' + value + '</span>' +
+                    '</li>';
+        }
+    };
+
+
+}();
 $(document).ready(function () {
 
     chrome.runtime.getBackgroundPage(function (bg) {
@@ -8,10 +56,6 @@ $(document).ready(function () {
         $('#jira_properties').empty();
 
         $.each(bg.backgroundData.applicationInfo, function (index, item) {
-
-            if(index === "isJira"){
-                return true;
-            }
 
             var color = '';
             if (item === true) {
